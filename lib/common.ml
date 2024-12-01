03a557b82c58ed3b525b027d7ee8c195d14b5ddd
Arnav Mummineni <45217840+RCoder01@users.noreply.github.com>
2024-12-01 16:36:01 -0500

    v2

--- lib/common.ml
+++ lib/common.ml
@@ -1,12 +1,13 @@
 let get_fname day is_test =
-    let day = Stdlib.string_of_int day in
-    let test_suffix = if is_test then "t" else "" in
-    "data/day" ^ day ^ test_suffix ^ ".txt"
+    "data/day" ^ Stdlib.string_of_int day ^ if is_test then "t" else "" ^ ".txt"
 
 let read_file fname =
     let chan = open_in fname in
     let len = in_channel_length chan in
-    let result = (Bytes.create len) in
+    let result = Bytes.create len in
     really_input chan result 0 len;
-    (Bytes.to_string result)
+    Bytes.to_string result
 
+let map2 f (a, b) = (f a, f b)
+
+let uncurry f (x, y) = f x y
--- lib/day1.ml
+++ lib/day1.ml
@@ -1,29 +1,26 @@
+open Common
+
 let parse_line line =
-    let substrs = String.split_on_char ' ' line in
-    let pairs = List.filter_map Stdlib.int_of_string_opt substrs in
+    let pairs = String.split_on_char ' ' line
+    |> List.filter_map Stdlib.int_of_string_opt in
     match pairs with
     | a :: b :: [] -> Some (a, b)
     | _ -> None
 
 let get_lists is_test =
-    let filename = Common.get_fname 1 is_test in
-    let str = Common.read_file filename in
-    let lines = String.split_on_char '\n' str in
-    let lines = List.filter (fun s -> (String.length s) <> 0) lines in
-    let halves = List.filter_map parse_line lines in
-    let (left, right) = List.split halves in
-    let compare a b = a - b in
-    let left = List.sort compare left in
-    let right = List.sort compare right in
-    (left, right)
+    get_fname 1 is_test
+    |> read_file
+    |> String.split_on_char '\n'
+    |> List.filter (fun s -> (String.length s) != 0)
+    |> List.filter_map parse_line
+    |> List.split
+    |> map2 (List.sort compare)
 
 let part1 is_test =
-    let (left, right) = get_lists is_test in
-    let diff a b = a - b in
-    let diffs = List.map2 diff left right in
-    let abs i = if i < 0 then -i else i in
-    let diffs = List.map abs diffs in
-    List.fold_left (fun acc e -> acc + e) 0 diffs
+    get_lists is_test
+    |> uncurry (List.map2 compare)
+    |> List.map abs
+    |> List.fold_left (+) 0
 
 let rec groupby func list =
     match list with
@@ -34,18 +31,15 @@ let rec groupby func list =
         | first :: rest -> (a :: first) :: rest
         | [] -> [[a]]
         )
-    | a :: [] -> [[a]]
-    | [] -> []
+    | l -> [l]
 
 let part2 is_test =
     let (left, right) = get_lists is_test in
-    let cmp a b = a - b in
-    let right = groupby cmp right in
-    let counts = List.map (fun l -> (List.hd l, List.length l)) right in
+    let counts = groupby compare right
+    |> List.map (fun l -> (List.hd l, List.length l)) in
     let get_score e =
-        let count_opt = List.assq_opt e counts in
-        let count = Option.value count_opt ~default:0 in
-        e * count in
-    let scores = List.map get_score left in
-    List.fold_left (fun a b -> a + b) 0 scores
-
+        List.assq_opt e counts
+        |> Option.value ~default:0
+        |> ( *) e in
+    List.map get_score left
+    |> List.fold_left (+) 0
